d7c6e9538da6893fbb5f3f821122ab9fec4f504e
sameerpandey18900-beep <sameerpandey18900@gmail.com>
2025-10-03 17:47:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ import asyncio
 
 TELEGRAM_TOKEN = os.environ.get("8379093665:AAFKQKg4K8Zsi0TS5b2p2evmSvbcBNSi_YQ")
 LITESHORT_API_KEY = os.environ.get("be1528376cd25a510dce1e3e063ed856e5421250")
-SERVER_URL = os.environ.get("https://testmcq.onrender.com/activate")  # e.g., https://yourapp.onrender.com/activate
+SERVER_URL = os.environ.get("SERVER_URL")  # e.g., https://yourapp.onrender.com/activate
 DB = "database.db"
 
 bot = Bot(TELEGRAM_TOKEN)
