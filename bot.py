a4904211f0bc5683f1a71d8b03b13323778c26b2
sameerpandey18900-beep <sameerpandey18900@gmail.com>
2025-10-03 16:20:42 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,5 @@
+import os
+
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+LITESHORT_API_KEY = os.environ.get("LITESHORT_API_KEY")
+SERVER_URL = os.environ.get("SERVER_URL")  # e.g., https://yourapp.koyeb.app/activate
