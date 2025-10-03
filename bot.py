3ee6463f4593a66a28f08a605cadf8a60b4a5049
sameerpandey18900-beep <sameerpandey18900@gmail.com>
2025-10-03 16:48:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,4 +2,4 @@ import os
 
 TELEGRAM_TOKEN = os.environ.get("8379093665:AAFKQKg4K8Zsi0TS5b2p2evmSvbcBNSi_YQ")
 LITESHORT_API_KEY = os.environ.get("be1528376cd25a510dce1e3e063ed856e5421250")
-SERVER_URL = os.environ.get("SERVER_URL")  # e.g., https://yourapp.koyeb.app/activate
+SERVER_URL = os.environ.get("https://testmcq.onrender.com")  # e.g., https://yourapp.koyeb.app/activate
