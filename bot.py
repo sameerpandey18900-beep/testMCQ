e3acc2c0da3796ae8ba36029ed996c9b9be23abd
sameerpandey18900-beep <sameerpandey18900@gmail.com>
2025-10-03 16:23:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import os
 
-TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
-LITESHORT_API_KEY = os.environ.get("LITESHORT_API_KEY")
+TELEGRAM_TOKEN = os.environ.get("8379093665:AAFKQKg4K8Zsi0TS5b2p2evmSvbcBNSi_YQ")
+LITESHORT_API_KEY = os.environ.get("be1528376cd25a510dce1e3e063ed856e5421250")
 SERVER_URL = os.environ.get("SERVER_URL")  # e.g., https://yourapp.koyeb.app/activate
